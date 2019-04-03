2e6c41488dd5c1be48230264e7a3c6def0db3582
patmalcolm91 <patmalcolm91@gmail.com>
2019-04-03 09:45:19 +0200

    Minor improvements
    Add description, disable automatic saving of layer changes, make input defaults general.

--- numberSelectedFeaturesGroupByField.py
+++ numberSelectedFeaturesGroupByField.py
@@ -1,11 +1,12 @@
 """
 Gives sequential numbers to each group of selected features in the active layer
 which have a matching value in a specified field.
+Written for QGIS 3. Tested in QGIS 2 and 3.
 Author: Patrick Malcolm
 """
 
-NUMBER_FIELD = "visumNOIdx"
-GROUP_FIELD = "HstbNummer"
+NUMBER_FIELD = "ID_FIELD"
+GROUP_FIELD = "GROUP"
 
 # ===============================================================
 
@@ -21,4 +22,4 @@ for feat in layer.selectedFeatures():
     feat.setAttribute(NUMBER_FIELD, nextNumber[g])
     layer.updateFeature(feat)
     nextNumber[g] += 1
-layer.commitChanges()
+#layer.commitChanges()  # uncomment this to automatically save changes
